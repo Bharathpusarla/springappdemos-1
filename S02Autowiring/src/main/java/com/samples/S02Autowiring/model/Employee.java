8ccb7c6e15134a5413ce8e10bb8f2c156632399f
Radhakrishnan Meiappan <rmeiappan@rocketsoftware.com>
2022-08-22 10:05:15 +0530

    changes

--- S02Autowiring/src/main/java/com/samples/S02Autowiring/model/Employee.java
+++ S02Autowiring/src/main/java/com/samples/S02Autowiring/model/Employee.java
@@ -1,37 +1,33 @@
 package com.samples.S02Autowiring.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Required;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Employee {
 
 	private int id;
 	private String name;
-	
-	private Address address;
-	
+
 	@Autowired
-	public Employee(Address address) {
-		this.address = address;
-	}
-	
+	@Qualifier("addressCal")
+	private Address address;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
-	
-	@Required
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
-	
+
 	@Override
 	public String toString() {
 		return "Employee [id=" + id + ", name=" + name + ", address=" + address + "]";
