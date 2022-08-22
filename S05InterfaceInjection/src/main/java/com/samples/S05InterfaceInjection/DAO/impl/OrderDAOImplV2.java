b27d71c4906fc095b4b22aec26682d8548acb943
Radhakrishnan Meiappan <rmeiappan@rocketsoftware.com>
2022-08-22 12:45:45 +0530

    changes

--- S05InterfaceInjection/src/main/java/com/samples/S05InterfaceInjection/DAO/impl/OrderDAOImplV2.java
+++ S05InterfaceInjection/src/main/java/com/samples/S05InterfaceInjection/DAO/impl/OrderDAOImplV2.java
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Component;
 
 import com.samples.S05InterfaceInjection.DAO.OrderDAO;
 
-@Component("dao")
-public class OrderDAOImpl implements OrderDAO{
+@Component("daov2")
+public class OrderDAOImplV2 implements OrderDAO{
 
 	@Override
 	public void createOrder() {
-		System.out.println("create order in mysql db");
+		System.out.println("create order in Oracle DB");
 	}
 
 }
